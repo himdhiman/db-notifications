ea0731462e74c2a470ebfbbd9f6be533cf158f55
Himanshu Dhiman <45459993+himdhiman@users.noreply.github.com>
2022-01-31 16:48:41 +0530

    updated

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Notification_List(models.Model):
+    message_type_choices = (("N", "Notify"), ("W", "Warning"))
+    message = models.TextField(blank=True, null=True)
+    message_type = models.CharField(choices=message_type_choices, max_length=20)
+    read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(default=now, editable=True)
+
+
+class Notification(models.Model):
+    user = models.EmailField(verbose_name="email", unique=True, max_length=60)
+    notifications = models.ManyToManyField(Notification_List)
